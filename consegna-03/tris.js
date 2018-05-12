2005dafb058a530be0639bd6b7674bc0f7636253
SamueleStefanello <samuele.stefanello@gmail.com>
2018-05-12 18:10:33 +0200

    Fixxato errori

--- consegna-03/tris.js
+++ consegna-03/tris.js
@@ -1,14 +1,17 @@
 let a =[0, 0, 0, 0, 0, 0, 0, 0, 0];
 let contatore = 0;
+let vittoria=0;
 
 function sres()
 {
+  $('#cover').css('visibility', 'visible');
   $('#reset').animate({
-    right: '+=20%',
-    top: '-=40%',
-    height: '+=70%',
-    width: '+=24%'} );
-    $('#reset').css('font-size', '400%');
+    right: '-=10%',
+    top: '-=24%',
+    height: '+=10%',
+    width: '+=12%'} );
+    $('#reset').css('font-size', '250%');
+
 }
 
 
@@ -36,6 +39,8 @@ $('#reset').animate({
   $('#win').css('visibility', 'hidden');
   $('#win').css('font-size', '175%');
   $('#faccina').css('visibility', 'hidden');
+  $('#cover').css('visibility', 'hidden');
+vittoria=0;
 };
 
 
@@ -46,7 +51,7 @@ let cerchio = "<img src=" + '"' + "cerchio.png" + '"' + "style=" + '"' + "width:
 let faccina = "<img src=" + '"' + "faccina.png" + '"' + "style=" + '"' + "width:80%;height:80%;" + '"' + "/>";
 
 function tris(casella) {
-    contatore++;
+  if(a[casella] == 0){      contatore++;}
     if (contatore%2 == 0 && a[casella] == 0) {
         a[casella] = 1;
     }
@@ -169,12 +174,11 @@ function tris(casella) {
           $('#reset').css('font-size', '400%');
         }*/
 
-
 switch((contatore%2)+1) {
     case 1:
-
     if(a[0]==(contatore%2)+1&&a[1]==(contatore%2)+1&&a[2]==(contatore%2)+1)
     {
+      vittoria=1;
       $('#1o').css('position','absolute');
       $('#1o').css('border-radius','15px');
       $('#1o').css('width','90%');
@@ -183,14 +187,12 @@ switch((contatore%2)+1) {
       $('#1o').css('left','5%');
       $('#1o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#1o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
-
     }
-    else if(a[3]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[5]==(contatore%2)+1)
-    {
+     if(a[3]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[5]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#2o').css('position','absolute');
       $('#2o').css('border-radius','15px');
       $('#2o').css('width','90%');
@@ -199,13 +201,12 @@ switch((contatore%2)+1) {
       $('#2o').css('left','5%');
       $('#2o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#2o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 }
-    else if(a[6]==(contatore%2)+1&&a[7]==(contatore%2)+1&&a[8]==(contatore%2)+1)
-    {
+    if(a[6]==(contatore%2)+1&&a[7]==(contatore%2)+1&&a[8]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#3o').css('position','absolute');
       $('#3o').css('width','90%');
       $('#3o').css('border-radius','15px');
@@ -214,13 +215,12 @@ switch((contatore%2)+1) {
       $('#3o').css('left','5%');
       $('#3o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#3o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 }
-    else if(a[0]==(contatore%2)+1&&a[3]==(contatore%2)+1&&a[6]==(contatore%2)+1)
-    {
+    if(a[0]==(contatore%2)+1&&a[3]==(contatore%2)+1&&a[6]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#1v').css('position','absolute');
       $('#1v').css('border-radius','15px');
       $('#1v').css('width','7%');
@@ -229,13 +229,12 @@ switch((contatore%2)+1) {
       $('#1v').css('left','15%');
       $('#1v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#1v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
-   }
-    else if(a[1]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[7]==(contatore%2)+1)
-    {
+      }
+    if(a[1]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[7]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#2v').css('position','absolute');
       $('#2v').css('border-radius','15px');
       $('#2v').css('width','7%');
@@ -244,13 +243,12 @@ switch((contatore%2)+1) {
       $('#2v').css('left','47%');
       $('#2v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#2v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
  }
-    else if(a[8]==(contatore%2)+1&&a[5]==(contatore%2)+1&&a[2]==(contatore%2)+1)
-    {
+    if(a[8]==(contatore%2)+1&&a[5]==(contatore%2)+1&&a[2]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#3v').css('position','absolute');
       $('#3v').css('border-radius','15px');
       $('#3v').css('width','7%');
@@ -259,13 +257,12 @@ switch((contatore%2)+1) {
       $('#3v').css('left','78%');
       $('#3v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#3v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 }
-    else if(a[6]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[2]==(contatore%2)+1)
-    {
+    if(a[6]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[2]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#t1').css('position','absolute');
       $('#t1').css('border-radius','15px');
       $('#t1').css('width','7%');
@@ -275,13 +272,12 @@ switch((contatore%2)+1) {
       $('#t1').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#t1').css('transform', 'rotate(45deg)');
       $('#t1').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 }
-    else if(a[0]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[8]==(contatore%2)+1)
-    {
+    if(a[0]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[8]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#t2').css('position','absolute');
       $('#t2').css('border-radius','15px');
       $('#t2').css('width','7%');
@@ -291,32 +287,31 @@ switch((contatore%2)+1) {
       $('#t2').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#t2').css('transform', 'rotate(315deg)');
       $('#t2').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 }
 
-      else if(a[0]!=0&&a[1]!=0&&a[2]!=0&&a[3]!=0&&a[4]!=0&&a[5]!=0&&a[6]!=0&&a[7]!=0&&a[8]!=0)
+      else if(a[0]!=0&&a[1]!=0&&a[2]!=0&&a[3]!=0&&a[4]!=0&&a[5]!=0&&a[6]!=0&&a[7]!=0&&a[8]!=0& vittoria==0)
       {
+        vittoria=1;
         document.getElementById("win").innerHTML ="PAREGGIO" ;
         $('#win').css('font-size', '+=100%');
         $('#win').css('visibility', 'visible');
         $('#faccina').css('visibility', 'visible');
-        window.setTimeout("sres();", 0);
-
-
-          break;
-
+        }
+        if (vittoria==1)
+        {
+          sres();
         }
 
-
+        break;
 
 
 
     case 2:
     if(a[0]==(contatore%2)+1&&a[1]==(contatore%2)+1&&a[2]==(contatore%2)+1)
-    {
+    {vittoria=1;
+
       $('#1o').css('position','absolute');
       $('#1o').css('border-radius','15px');
       $('#1o').css('width','90%');
@@ -325,14 +320,13 @@ switch((contatore%2)+1) {
       $('#1o').css('left','5%');
       $('#1o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#1o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
 
     }
-    else if(a[3]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[5]==(contatore%2)+1)
-    {
+    if(a[3]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[5]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#2o').css('position','absolute');
       $('#2o').css('border-radius','15px');
       $('#2o').css('width','90%');
@@ -341,13 +335,12 @@ switch((contatore%2)+1) {
       $('#2o').css('left','5%');
       $('#2o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#2o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
   }
-    else if(a[6]==(contatore%2)+1&&a[7]==(contatore%2)+1&&a[8]==(contatore%2)+1)
-    {
+    if(a[6]==(contatore%2)+1&&a[7]==(contatore%2)+1&&a[8]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#3o').css('position','absolute');
       $('#3o').css('width','90%');
       $('#3o').css('border-radius','15px');
@@ -356,13 +349,12 @@ switch((contatore%2)+1) {
       $('#3o').css('left','5%');
       $('#3o').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#3o').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
   }
-    else if(a[0]==(contatore%2)+1&&a[3]==(contatore%2)+1&&a[6]==(contatore%2)+1)
-    {
+    if(a[0]==(contatore%2)+1&&a[3]==(contatore%2)+1&&a[6]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#1v').css('position','absolute');
       $('#1v').css('border-radius','15px');
       $('#1v').css('width','7%');
@@ -371,14 +363,13 @@ switch((contatore%2)+1) {
       $('#1v').css('left','15%');
       $('#1v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#1v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
 
-      window.setTimeout("sres();", 2000);
-      break;
    }
-    else if(a[1]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[7]==(contatore%2)+1)
-    {
+    if(a[1]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[7]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#2v').css('position','absolute');
       $('#2v').css('border-radius','15px');
       $('#2v').css('width','7%');
@@ -387,13 +378,12 @@ switch((contatore%2)+1) {
       $('#2v').css('left','47%');
       $('#2v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#2v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
   }
-    else if(a[8]==(contatore%2)+1&&a[5]==(contatore%2)+1&&a[2]==(contatore%2)+1)
-    {
+    if(a[8]==(contatore%2)+1&&a[5]==(contatore%2)+1&&a[2]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#3v').css('position','absolute');
       $('#3v').css('border-radius','15px');
       $('#3v').css('width','7%');
@@ -402,13 +392,12 @@ switch((contatore%2)+1) {
       $('#3v').css('left','78%');
       $('#3v').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#3v').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
   }
-    else if(a[6]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[2]==(contatore%2)+1)
-    {
+    if(a[6]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[2]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#t1').css('position','absolute');
       $('#t1').css('border-radius','15px');
       $('#t1').css('width','7%');
@@ -418,13 +407,12 @@ switch((contatore%2)+1) {
       $('#t1').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#t1').css('transform', 'rotate(45deg)');
       $('#t1').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
-      break;
   }
-    else if(a[0]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[8]==(contatore%2)+1)
-    {
+    if(a[0]==(contatore%2)+1&&a[4]==(contatore%2)+1&&a[8]==(contatore%2)+1)
+    {vittoria=1;
+
       $('#t2').css('position','absolute');
       $('#t2').css('border-radius','15px');
       $('#t2').css('width','7%');
@@ -434,23 +422,24 @@ switch((contatore%2)+1) {
       $('#t2').css('background', 'linear-gradient(to right, #FF0000 , yellow)');
       $('#t2').css('transform', 'rotate(315deg)');
       $('#t2').css('visibility', 'visible');
-      document.getElementById("win").innerHTML ="WINNER PLAYER X" ;
+      document.getElementById("win").innerHTML ="WINNER PLAYER O" ;
       $('#win').css('visibility', 'visible');
-      window.setTimeout("sres();", 2000);
 
-      break;
   }
 
-      else if(a[0]!=0&&a[1]!=0&&a[2]!=0&&a[3]!=0&&a[4]!=0&&a[5]!=0&&a[6]!=0&&a[7]!=0&&a[8]!=0)
-      {
+      else if(a[0]!=0&&a[1]!=0&&a[2]!=0&&a[3]!=0&&a[4]!=0&&a[5]!=0&&a[6]!=0&&a[7]!=0&&a[8]!=0& vittoria==0)
+      {vittoria=1;
+
         document.getElementById("win").innerHTML ="PAREGGIO" ;
         $('#win').css('font-size', '+=100%');
         $('#win').css('visibility', 'visible');
         $('#faccina').css('visibility', 'visible');
-        window.setTimeout("sres();", 0);
-
-          break;
-
         }
+        if (vittoria==1)
+        {
+          sres();
+        }
+        break;
+
 }
 };
